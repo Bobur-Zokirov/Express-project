4ffb2ffef50832b3e77d1359ad8dac9f5a8e9885
Bobur Zokirov <zeromegamaks@gmail.com>
2023-05-14 13:23:25 +0500

    MongoDB connected

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from "express";
 import { create } from "express-handlebars";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Routes
 import AuthRouter from "./routes/auth.js";
 import ProductsRouter from "./routes/products.js";
 
@@ -13,9 +19,28 @@ app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
 
 app.use(AuthRouter);
 app.use(ProductsRouter);
 
-const PORT = process.env.PORT || 4100;
-app.listen(PORT, () => console.log(`Server ${PORT} chi portda ishga tushdi`));
+const startApp = () => {
+  try {
+    mongoose.connect(
+      process.env.MONGO_URI,
+      {
+        useNewUrlParser: true,
+      },
+      console.log("mongodb ulandi...")
+    );
+
+    const PORT = process.env.PORT || 4100;
+    app.listen(PORT, () =>
+      console.log(`Server ${PORT} chi portda ishga tushdi`)
+    );
+  } catch (error) {
+    console.log("Xatolik yuz berdi...", error);
+  }
+};
+
+startApp();
