63c241b5952f3439b77d1008df3eadeadde8395f
Bobur Zokirov <zeromegamaks@gmail.com>
2023-05-15 10:14:14 +0500

    Validation (Register & Login)

--- routes/auth.js
+++ routes/auth.js
@@ -4,52 +4,77 @@ import User from "../models/User.js";
 
 const router = Router();
 
+// ---------- LOGUN GET ----------- //
+
 router.get("/login", (req, res) => {
   res.render("login", {
     title: "Login | Shop",
     isLogin: true,
+    loginError: req.flash("loginError"),
   });
 });
+
+// ---------- REGISTER GET ----------- //
 
 router.get("/register", (req, res) => {
   res.render("register", {
     title: "Register | Shop",
     isRegister: true,
+    registerError: req.flash("registerError"),
   });
 });
 
+// ---------- REGISTER POST ----------- //
+
 router.post("/register", async (req, res) => {
-  // console.log(req.body);
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const { firstname, lastname, email, password } = req.body;
+  if (!firstname || !lastname || !email || !password) {
+    req.flash("registerError", "All fields is required");
+    res.redirect("/register");
+    return;
+  }
+
+  const isEmailExist = await User.findOne({ email });
+  if (isEmailExist) {
+    req.flash("registerError", "User already exist!");
+    res.redirect("/register");
+    return;
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
   const userData = {
-    firstName: req.body.firstname,
-    lastName: req.body.lastname,
-    email: req.body.email,
+    firstName: firstname,
+    lastName: lastname,
+    email: email,
     password: hashedPassword,
   };
   const user = await User.create(userData);
   res.redirect("/");
 });
 
+// ---------- LOGIN POST ----------- //
+
 router.post("/login", async (req, res) => {
-  // console.log(req.body);
+  const { email, password } = req.body;
 
-  const existUser = await User.findOne({ email: req.body.email });
+  if (!email || !password) {
+    req.flash("loginError", "All fields is required");
+    res.redirect("/login");
+    return;
+  }
+
+  const existUser = await User.findOne({ email });
   if (!existUser) {
-    console.log("User not Found");
+    req.flash("loginError", "User not found");
+    res.redirect("/login");
     return;
   }
 
-  const isPassworMatch = await bcrypt.compare(
-    req.body.password,
-    existUser.password
-  );
+  const isPassworMatch = await bcrypt.compare(password, existUser.password);
   if (!isPassworMatch) {
-    console.log("Password is wrong");
+    req.flash("loginError", "Password is wrong");
+    res.redirect("/login");
     return;
   }
-
-  console.log(existUser);
 
   res.redirect("/");
 });
