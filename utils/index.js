027130bd7465bc51f69f897dd19cfaba77ee7aef
Bobur Zokirov <zeromegamaks@gmail.com>
2023-07-05 16:53:06 +0500

    #26. Qo'shimcha funksiyalar

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+import moment from "moment/moment.js";
+
 export default {
   ifequal(a, b, options) {
     if (a == b) {
@@ -8,5 +10,13 @@ export default {
 
   getNameFirstChar(firstname, lastname) {
     return firstname.charAt(0) + lastname.charAt(0);
+  },
+
+  createDate(date) {
+    return moment(date).format("DD MMM, YYYY");
+  },
+
+  updateDate(date) {
+    return moment(date).fromNow();
   },
 };
