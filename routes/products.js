a51d4d0525d2a7eb53abfbc4945a13ab2fb2040e
Bobur Zokirov <zeromegamaks@gmail.com>
2023-07-05 18:53:20 +0500

    #28. Edit Product

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,6 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   const products = await Product.find().populate("user").lean();
-  // console.log(products);
   res.render("index", {
     title: "Boom | Shop",
     products: products.reverse(),
@@ -36,7 +35,6 @@ router.get("/product/:id", async (req, res) => {
   res.render("product", {
     title: "Product Detail | Shop",
     product: product,
-    // isProducts: true,
   });
 });
 
@@ -58,6 +56,29 @@ router.post("/add-products", async (req, res) => {
 
   await Product.create({ ...req.body, user: req.userId });
   res.redirect("/");
+});
+
+router.get("/edit-product/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id).lean();
+  res.render("edit-product", {
+    title: "Edit Product",
+    product: product,
+    editProductError: req.flash("editProductError"),
+  });
+});
+
+router.post("/edit-product/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, description, image, price } = req.body;
+  if (!title || !description || !image || !price) {
+    req.flash("editProductError", "All fields is required");
+    res.redirect(`/edit-product/${id}`);
+    return;
+  }
+
+  await Product.findByIdAndUpdate(id, req.body, { new: true });
+
+  res.redirect(`/product/${id}`);
 });
 
 export default router;
